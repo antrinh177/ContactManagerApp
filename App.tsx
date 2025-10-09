d8add01e8807aa3e0f3b191ac4efaae2de33509f
Ân Trịnh Thị Minh <antrinh@Ans-MacBook-Pro.local>
2025-10-09 11:23:19 -0400

    add cmts and apply performance optimization

--- App.tsx
+++ App.tsx
@@ -1,38 +1,39 @@
-// src/App.tsx
-
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 import {
   Text,
-  SafeAreaView
+  SafeAreaView,
+  ActivityIndicator
 } from 'react-native';
 
-// Screens
-import AddContactScreen from './src/screens/AddContact/AddContactScreen';
-import ContactListScreen from './src/screens/ContactList/ContactListScreen';
-import ContactDetailsScreen from './src/screens/ContactDetails/ContactDetailsScreen';
+// Lazy load screens
+const AddContactScreen = React.lazy(() => import('./src/screens/AddContact/AddContactScreen'));
+const ContactListScreen = React.lazy(() => import('./src/screens/ContactList/ContactListScreen'));
+const ContactDetailsScreen = React.lazy(() => import('./src/screens/ContactDetails/ContactDetailsScreen'));
 
-// Context
+// Utils components
 import { ContactProvider } from './src/utils/ContactContext';
 
-
-
-
 const App = () => {
+  // Use navigation stack to manage state.
   const [stack, setStack] = useState([{ name: 'ContactList', params: {} }]);
 
+  // Function to move to new screen by adding new screen to the stack
   const navigate = (name: any, params = {}) => {
     setStack(prevStack => [...prevStack, { name, params }]);
   };
 
+  // Function to go back to previous screen by removing the top screen from the stack
   const goBack = () => {
     setStack(prevStack => (prevStack.length > 1 ? prevStack.slice(0, -1) : prevStack));
   };
 
+  // Render the current screen based on the top of the stack
   const renderScreen = () => {
     const currentScreen = stack[stack.length - 1];
     const navigation = { navigate, goBack };
     const route = { params: currentScreen.params };
 
+    // Condition to render the appropriate screen component
     switch (currentScreen.name) {
       case 'ContactList':
         return <ContactListScreen navigation={navigation} />;
@@ -49,9 +50,12 @@ const App = () => {
     }
   };
 
+  // Wrap the app in ContactProvider so all screens have access to shared context
   return (
     <ContactProvider>
-      {renderScreen()}
+      <Suspense fallback={<ActivityIndicator size="large" color="#0000ff" />}>
+        {renderScreen()}
+      </Suspense>
     </ContactProvider>
   );
 };
